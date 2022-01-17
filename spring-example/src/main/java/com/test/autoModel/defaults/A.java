9931a9b17d1e3656308d0f5e58385d0113bb1b76
sunyunfeng <67753460@qq.com>
2022-01-17 23:26:22 +0800

    spring注入时,注入模型的影响

--- spring-example/src/main/java/com/test/autoModel/defaults/A.java
+++ spring-example/src/main/java/com/test/autoModel/defaults/A.java
@@ -2,33 +2,37 @@ package com.test.autoModel.defaults;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-@Component
+@Component("aa")
 @Slf4j(topic = "e")
-public class A implements ExampleAware {
+public class A  implements ExampleAware{
 
 	ExampleBean exampleBean;
 
-	public A() {
-		log.debug("default Constructor");
+	public A(){
+		log.debug("default Constructor a");
 
 	}
 
 
-//
 
-
-//	public A(C c,B b,ExampleBean exampleBean){
-//		log.debug("Constructor from c b,exampleBean");
-//		this.exampleBean =exampleBean;
-//	}
-
-	public ExampleBean getExampleBean() {
-		return exampleBean;
+	public A(C c,B b,ExampleBean exampleBean){
+		log.debug("Constructor from c b,exampleBean");
+		//this.exampleBean =exampleBean;
 	}
 
+	public A(C c) {
+		log.debug("default Constructor c");
+	}
+
+	public A(B b) {
+		log.debug("default Constructor b");
+	}
 	@Override
 	public void setExampleBean(ExampleBean exampleBean) {
 		//this.exampleBean=exampleBean;
+	}
+
+	public ExampleBean getExampleBean() {
+		return exampleBean;
 	}
 }
--- spring-example/src/main/java/com/test/autoModel/util/ModelBeanFactoryPostProcessor.java
+++ spring-example/src/main/java/com/test/autoModel/util/ModelBeanFactoryPostProcessor.java
@@ -1,5 +1,6 @@
 package com.test.autoModel.util;
 
+import com.test.autoModel.defaults.ExampleBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
@@ -10,10 +11,16 @@ import org.springframework.beans.factory.support.AbstractBeanDefinition;
 public class ModelBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 	@Override
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-		AbstractBeanDefinition beanDefinition = (AbstractBeanDefinition) beanFactory.getBeanDefinition("m");
-		//获取注入模型
-		log.debug("mode:{}", beanDefinition.getAutowireMode());
-		//手动设置注入模型
-		beanDefinition.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_BY_TYPE);
+		AbstractBeanDefinition beanDefinition = (AbstractBeanDefinition) beanFactory.getBeanDefinition("aa");
+
+		log.debug("mode:{}",beanDefinition.getAutowireMode());
+		//beanDefinition.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_BY_TYPE);
+
+		//给我们A对象设置他的注入模型 =3
+		// 3 首先参数必须注入到spring中, 然后当参数数量相同,选择最后的. 当数量不同, 选择最多的
+		beanDefinition.setAutowireMode(3);
+		//beanDefinition.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_BY_TYPE);
+		//a.getPropertyValues().add("exampleBean",beanFactory.getBean("exampleBean"));
+		//beanFactory.ignoreDependencyType(ExampleBean.class);
 	}
 }
